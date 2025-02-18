06bc26f570283856d440db22a6e9e629adf10e73
WinterAtne <104174438+WinterAtne@users.noreply.github.com>
2025-02-17 20:49:51 -0700

    Never mind we're making a different game

--- asssets/resource_definitions/item.gd
+++
@@ -1,5 +0,0 @@
-class_name Item
-extends Resource
-
-@export var name : StringName
-@export var icon : Image
--- tmp/ship.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-@export var inventory : Inventory
-@export var itm : Item
-
-func _ready() -> void:
-	inventory = $Inventory
-	while !inventory.is_full():
-		inventory.add(itm)
