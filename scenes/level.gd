4fd9bc02d453d92eb103320cb5f2f4712c11b852
WinterAtne <104174438+WinterAtne@users.noreply.github.com>
2025-02-20 16:15:24 -0700

    Added random spawning

--- scenes/level.gd
+++ scenes/level.gd
@@ -5,11 +5,23 @@ signal game_over
 
 static var instance : Level = null
 
+@export var enemy_types : Array[PackedScene] = []
+
 func _init() -> void:
 	if instance == null:
 		instance = self
 	else:
 		self.queue_free()
+
+func spawn_enemies() -> void:
+	var template : PackedScene = enemy_types.pick_random()
+	var spawned_enemy = template.instantiate()
+	add_child(spawned_enemy)
+	
+	var direction = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
+	spawned_enemy.position = randf_range(800, 1200) * direction + Player.instance.global_position
+	
+	
 
 func end_game() -> void:
 	game_over.emit()
