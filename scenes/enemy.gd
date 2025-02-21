20c084b27506ba49b34c6f54fa1eb1b03c051c3d
WinterAtne <104174438+WinterAtne@users.noreply.github.com>
2025-02-20 20:27:51 -0700

    Made enemies quasi-boids

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -3,8 +3,26 @@ extends CharacterBody2D
 
 const SPEED : int = 600
 
+var min_distance : float = 200
+var boid_behavior : float = 0.50
+var neighbors : Array[BoidArea] = []
+
+var target_direction : Vector2 = Vector2.ZERO
 func _physics_process(delta: float) -> void:
-	var target_direction : Vector2 = global_position.direction_to(Player.instance.global_position)
+	
+	var coherence : Vector2
+	for neighbor in neighbors:
+		var direction = global_position.direction_to(neighbor.global_position)
+		var distance = global_position.distance_to(neighbor.global_position)
+		if distance < min_distance:
+			coherence -= direction * distance
+		else:
+			coherence += direction / distance
+	
+	target_direction = target_direction.move_toward(
+		(global_position.direction_to(Player.instance.global_position) * (1 - boid_behavior) +
+		coherence * boid_behavior).normalized(),
+		delta)
 	
 	velocity = velocity.move_toward(target_direction * SPEED, SPEED * delta * Engine.physics_ticks_per_second)
 	
@@ -15,3 +33,11 @@ func die() -> void:
 
 func _on_damageable_died() -> void:
 	die()
+
+func _on_boid_area_area_entered(area: Area2D) -> void:
+	if area is BoidArea:
+		neighbors.append(area)
+
+func _on_boid_area_area_exited(area: Area2D) -> void:
+	if area is BoidArea and neighbors.has(area):
+		neighbors.erase(area)
