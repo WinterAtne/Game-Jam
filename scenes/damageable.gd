779c2f43f6398f68de4d8b9dcb095867f000c777
WinterAtne <104174438+WinterAtne@users.noreply.github.com>
2025-02-20 19:18:22 -0700

    Added brief invulnerability for player

--- scenes/damageable.gd
+++ scenes/damageable.gd
@@ -4,11 +4,13 @@ extends Area2D
 signal took_damage(attacker : Damager, new_health : int, direction : Vector2)
 signal died()
 
+@export var invulnerable_time : float = 0
 @export var max_health : int
 @onready var current_health : int = max_health
 
 func _init() -> void:
 	area_entered.connect(_on_area_entered)
+	
 
 func _on_area_entered(attacker: Area2D) -> void:
 	if attacker is Damager:
@@ -17,4 +19,13 @@ func _on_area_entered(attacker: Area2D) -> void:
 			died.emit()
 		else:
 			took_damage.emit(attacker, current_health, global_position.direction_to(attacker.global_position))
-	
+		
+		if invulnerable_time > 0:
+			(func(): monitoring = false; monitorable = false).call_deferred()
+			
+			get_tree().create_timer(invulnerable_time).timeout.connect(
+				(func(): monitoring = true; monitorable = true),
+				CONNECT_DEFERRED
+			)
+			
+		
