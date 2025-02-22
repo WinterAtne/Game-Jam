2259809b7d809fd4e07d72f1265addf51563a414
WinterAtne <104174438+WinterAtne@users.noreply.github.com>
2025-02-22 14:26:35 -0700

    Player status now declares "JAM" when gun jams

--- scenes/ui/player_status.gd
+++ scenes/ui/player_status.gd
@@ -1,4 +1,17 @@
 extends CanvasLayer
 
+const display_jamed_time : float = 0.2
+
 func set_health(new_health : int) -> void:
 	%HealthBar.value = new_health
+
+func display_jam() -> void:
+	if %Jam.visible:
+		pass
+	
+	%Jam.visible = true
+	get_tree().create_timer(display_jamed_time).timeout.connect(
+		(func(): %Jam.visible = false),
+		CONNECT_DEFERRED
+		)
+	
