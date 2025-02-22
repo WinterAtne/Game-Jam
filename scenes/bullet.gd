6bf8d1eae3f76b82dee731144bae0e4c3b0cf502
WinterAtne <104174438+WinterAtne@users.noreply.github.com>
2025-02-22 14:50:14 -0700

    Made modifications to bullets, fixing interpolation issues, and freeing them on hit

--- scenes/bullet.gd
+++ scenes/bullet.gd
@@ -1,13 +1,18 @@
 class_name Bullet
-extends RigidBody2D
+extends Node2D
 
 @export var mussle_velocity : float
+var direction : Vector2 = Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	position += direction * mussle_velocity * delta
 
 func fire(dir : Vector2, pos : Vector2) -> void:
 	position = pos
-	apply_impulse(dir * mussle_velocity)
-	pass
-
+	direction = dir
 
 func die() -> void:
 	self.queue_free()
+
+func _on_damager_did_damage() -> void:
+	die()
--- scenes/damager.gd
+++ scenes/damager.gd
@@ -1,5 +1,14 @@
 class_name Damager
 extends Area2D
 
+signal did_damage()
+
 @export var damage : int = 1
 @export var knockback : float = 200
+
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+
+func _on_area_entered(area : Area2D) -> void:
+	if area is Damageable:
+		did_damage.emit()
